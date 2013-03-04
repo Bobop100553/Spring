ef39479462177a8e0c40d064bcfedbd2aa8797f3
Bob Piper <bob.piper1953@virginmedia.com>
2013-03-04 10:36:15 +0000

    Instantiate and draw a Triangle using a Spring Bean Factory object
    Signed-off-by: Bob Piper <bob.piper1953@virginmedia.com>

--- SpringDemo/src/org/koushik/javabrains/DrawingApp.java
+++ SpringDemo/src/org/koushik/javabrains/DrawingApp.java
@@ -1,5 +1,9 @@
 package org.koushik.javabrains;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
 public class DrawingApp {
 
 	/**
@@ -8,7 +12,12 @@ public class DrawingApp {
 	public static void main(String[] args) {
 
 		// Instantiate and draw a Triangle without using Spring
-		Triangle triangle = new Triangle();
+		// Triangle triangle = new Triangle();
+		//triangle.draw();
+		
+		// Instantiate and draw a Triangle using a Spring Bean Factory object
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		Triangle triangle = (Triangle) factory.getBean("Triangle");
 		triangle.draw();
 	}
 
