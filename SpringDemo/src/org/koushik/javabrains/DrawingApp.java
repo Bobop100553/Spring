2c8705c4771311387052cf1557839911e8e4e6ed
Bob Piper <bob.piper1953@virginmedia.com>
2013-03-20 01:20:40 +0000

    Part 22(2) - "Using MessageSource to Get Text From Property Files".
    Externalize all messages and provide 4 different language translations.
    
    Signed-off-by: Bob Piper <bob.piper1953@virginmedia.com>

--- SpringDemo/src/org/koushik/javabrains/DrawingApp.java
+++ SpringDemo/src/org/koushik/javabrains/DrawingApp.java
@@ -1,21 +1,34 @@
 package org.koushik.javabrains;
 
+import java.util.Locale;
+
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
 	private static AbstractApplicationContext context;
+	private static Locale locale;
+	
+	public static Locale getLocale() {
+		return locale;
+	}
+
+	public static void setLocale(Locale locale) {
+		DrawingApp.locale = locale;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
+		locale = Locale.FRANCE;
+		
 		// Instantiate and draw a Triangle using an Application Context object
 		context = new ClassPathXmlApplicationContext("spring.xml");
 		context.registerShutdownHook();
-		System.out.println(context.getMessage("application.greeting", null, "Hello!", null));
+		System.out.println(context.getMessage("application.greeting", null, "Hello!", locale));
 		Shape shape1 = (Shape) context.getBean("shape1");
 		shape1.draw();
 		System.out.println();
