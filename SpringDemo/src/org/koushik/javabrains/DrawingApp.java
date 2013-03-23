e401c409d5f6f59a5bad79ce4c243d0a5e980381
Bob Piper <bob.piper1953@virginmedia.com>
2013-03-23 21:28:28 +0000

    Part 4.0 - "Writing Code Using the Bean Factory".
    Create, instantiate and draw a triangle (no Spring)
    
    Create a triangle class. Instantiate a triangle in the DrawingApp class
    and call its draw method.
    
    Signed-off-by: Bob Piper <bob.piper1953@virginmedia.com>

---
+++ SpringDemo/src/org/koushik/javabrains/DrawingApp.java
@@ -0,0 +1,15 @@
+package org.koushik.javabrains;
+
+public class DrawingApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Instantiate and draw a Triangle without using Spring
+		Triangle triangle = new Triangle();
+		triangle.draw();
+	}
+
+}
