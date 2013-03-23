e89c9d50f9a817d0bd8b6fa65417a654fe6fac21
Bob Piper <bob.piper1953@virginmedia.com>
2013-03-23 22:00:20 +0000

    Part 6.1 - "Using Constructor Injection".
    Set triangle type to "Equilateral" and triangle height to 20 using
    constructor injection.
    
    Add constructor-arg for triangle height to spring.xml.
    
    Signed-off-by: Bob Piper <bob.piper1953@virginmedia.com>

--- SpringDemo/src/org/koushik/javabrains/DrawingApp.java
+++ SpringDemo/src/org/koushik/javabrains/DrawingApp.java
@@ -1,10 +1,7 @@
 package org.koushik.javabrains;
 
-// import org.springframework.beans.factory.BeanFactory;
-// import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-// import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
@@ -15,15 +12,7 @@ public class DrawingApp {
 	 */
 	public static void main(String[] args) {
 
-		// Instantiate and draw a Triangle without using Spring
-		// Triangle triangle = new Triangle();
-		// triangle.draw();
-		
-		// Instantiate and draw a Triangle using a Spring Bean Factory object
-		/* BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		   Triangle triangle = (Triangle) factory.getBean("Triangle");
-		   triangle.draw(); */
-		
+		// Instantiate and draw a Triangle using an Application Context object
 		context = new ClassPathXmlApplicationContext("spring.xml");
 		Triangle triangle = (Triangle) context.getBean("Triangle");
 		triangle.draw();
--- SpringDemo/src/org/koushik/javabrains/Triangle.java
+++ SpringDemo/src/org/koushik/javabrains/Triangle.java
@@ -5,18 +5,8 @@ public class Triangle {
 	private String type;
 	private int height;
 	
-	public Triangle() {
-		
-	}
-	
 	public Triangle(String type) {
-		super();
 		this.type = type;
-	}
-
-	public Triangle(int height) {
-		super();
-		this.height = height;
 	}
 
 	public Triangle(String type, int height) {
@@ -29,16 +19,8 @@ public class Triangle {
 		return type;
 	}
 
-	public void setType(String type) {
-		this.type = type;
-	}
-
 	public int getHeight() {
 		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
 	}
 
 	public void draw() {
