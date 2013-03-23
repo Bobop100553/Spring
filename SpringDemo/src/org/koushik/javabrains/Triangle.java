cd0cf9c8c3a294fcdd743240b0d91f74f4daaf27
Bob Piper <bob.piper1953@virginmedia.com>
2013-03-23 22:21:16 +0000

    Part 9.0 - "Initialising Collections".
    Create triangle using a list of points.
    
    Amend triangle class by removing the three individual points and
    replacing with a list of points. Also amend the draw method to print
    each point within a for...each loop.
    Amend spring.xml by removing the three individual points and replacing
    with a list. The list contains three references to point objects.
    
    Signed-off-by: Bob Piper <bob.piper1953@virginmedia.com>

--- SpringDemo/src/org/koushik/javabrains/Triangle.java
+++ SpringDemo/src/org/koushik/javabrains/Triangle.java
@@ -1,40 +1,23 @@
 package org.koushik.javabrains;
 
+import java.util.List;
+
 public class Triangle {
 	
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
+	List<Point> points;
 	
-	public Point getPointA() {
-		return pointA;
+	public List<Point> getPoints() {
+		return points;
 	}
 
-	public void setPointA(Point pointA) {
-		this.pointA = pointA;
-	}
-
-	public Point getPointB() {
-		return pointB;
-	}
-
-	public void setPointB(Point pointB) {
-		this.pointB = pointB;
-	}
-
-	public Point getPointC() {
-		return pointC;
-	}
-
-	public void setPointC(Point pointC) {
-		this.pointC = pointC;
+	public void setPoints(List<Point> points) {
+		this.points = points;
 	}
 
 	public void draw() {
-		System.out.println("Triangle drawn.");
-		System.out.println("Point A: (" + pointA.getX() + ", " + pointA.getY() + ")");
-		System.out.println("Point B: (" + pointB.getX() + ", " + pointB.getY() + ")");
-		System.out.println("Point C: (" + pointC.getX() + ", " + pointC.getY() + ")");
+		for (Point point : points) {
+			System.out.println("Point = (" + point.getX() + ", " + point.getY() + ")");
+		}
 	}
 	
 }
